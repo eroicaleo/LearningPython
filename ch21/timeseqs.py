a6a8ba92ea3e4866b228d8d531c4d02db9d7cc58
Yang Ge <eroicaleo@gmail.com>
2014-04-27 00:08:13 -0700

    Added test code for timer2.py

--- ch21/timeseqs.py
+++ ch21/timeseqs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, timer
+import sys, timer, timer2
 
 reps = 10000
 repslist = list(range(reps))
@@ -31,4 +31,9 @@ print(sys.version)
 for test in (forLoop, listComp, mapCall, genExpr, genFunc):
     (bestof, (total, result)) = timer.bestoftotal(5, 1000, test)
     print('%-9s : %.5f => [%s..%s] ' % (test.__name__, bestof, result[0], result[-1]))
+
+for test in (forLoop, listComp, mapCall, genExpr, genFunc):
+    (total, result) = timer2.bestoftotal(test, _reps1=5, _reps=1000)
+    print('%-9s : %.5f => [%s..%s] ' % (test.__name__, total, result[0], result[-1]))
+
 
