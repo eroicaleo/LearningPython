16a12b17cb3483ab343bd3732535e6380bafe732
Yang Ge <eroicaleo@gmail.com>
2014-09-17 15:55:36 -0700

    Added module2.py and namespace.py

---
+++ ch23/module2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+print('Start loading')
+import sys
+name = 42
+def func(): pass
+class kclass: pass
+print('End loading')
---
+++ ch23/namespace.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+import module2
+print(module2.sys)
+print(module2.name)
+print(module2.func)
+print(module2.kclass)
