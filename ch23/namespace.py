03ed452e9abee865c3cbba6e664319be7087b7db
Yang Ge <eroicaleo@gmail.com>
2014-09-17 17:48:00 -0700

    Updates on namespace.py

--- ch23/namespace.py
+++ ch23/namespace.py
@@ -5,3 +5,8 @@ print(module2.sys)
 print(module2.name)
 print(module2.func)
 print(module2.kclass)
+
+print(list(module2.__dict__.keys()))
+print("User defined names:")
+print([name for name in module2.__dict__ if not name.startswith('__')])
+print(module2.name, module2.__dict__['name'])
