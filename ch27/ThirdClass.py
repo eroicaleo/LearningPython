e97c04e7e6987a1ed30b13c782664311b53b619b
Yang Ge <eroicaleo@gmail.com>
2015-05-16 16:35:40 -0700

    updates ThirdClass.py

--- ch27/ThirdClass.py
+++ ch27/ThirdClass.py
@@ -11,6 +11,14 @@ class ThirdClass(SecondClass.SecondClass):
     def __str__(self):
         return '[Third class: %s]' % (self.data)
 
+    def mult(self, n):
+        self.data = self.data * n
+
 a = ThirdClass('abc')
 a.display()
+print(a)
+
+b = a + 'xyz'
+print(b)
+a.mult(3)
 print(a)
